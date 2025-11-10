de186603ed1c526a11319ee34ef43ac5e635d43e
pradeepa2111 <pradeepa2111@gmail.com>
2025-11-10 10:43:55 +0530

    Add files via upload
    week1.day3

---
+++ day3/Chrome.java
@@ -0,0 +1,30 @@
+package week1.day3;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float version = 91.0f;
+String developer ="Google";
+boolean isBeta = false;
+int releaseYear =2008;
+char shortcutKey ='C';
+{
+	System.out.println("version = "+version);
+	System.out.println("developer ="+developer);
+	System.out.println("isBeta = "+isBeta);
+	System.out.println("releaseYear = "+releaseYear);
+	System.out.println("shortcutKey = "+shortcutKey);
+}
+
+
+	}
+
+}
+
+
+/*1. version of type float with a value of 91.0f
+2. developer of type String with a value of "Google"
+3. isBeta of type boolean with a value of false
+4. releaseYear of type int with a value of 2008
+5. shortcutKey of type char with a value of 'C'*/
---
+++ day3/FindOddNumbers.java
@@ -0,0 +1,17 @@
+package week1.day3;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++)
+		{
+			if (i%2==1)
+			{
+				System.out.println("The number is " +i+" odd");
+			}
+		}
+		
+			}
+		
+		}
